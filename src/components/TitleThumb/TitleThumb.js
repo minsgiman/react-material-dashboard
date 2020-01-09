fb2dc5cd8feae5741d55f6a7ee25c45e6bc63ea8
mskang <minsgiman@gmail.com>
2020-01-09 22:09:03 +0900

    photo viewer 구현

--- src/components/TitleThumb/TitleThumb.js
+++ src/components/TitleThumb/TitleThumb.js
@@ -14,10 +14,10 @@ const TitleThumb = props => {
     <div className="title_thumb" onClick={() => thumbClickCb(id)}>
       <div className="img_box">
         <img src={imgUrl}></img>
-      </div>
-      <div className="inner_info">
-        <span className="date_info">{startDate}</span>
-        <span className="tit_info">{title}</span>
+        <div className="inner_info">
+          <span className="date_info">{startDate}</span>
+          <span className="tit_info">{title}</span>
+        </div>
       </div>
       <div className="desc_wrap">
         <p>{description}</p>
